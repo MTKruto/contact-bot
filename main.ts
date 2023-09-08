beeb4fe4ea9a149c0cd0a28932bbf9b7f0da4358
Roj <ez@roj.im>
2023-09-08 22:17:17 +0300

    Update MTKruto

--- main.ts
+++ main.ts
@@ -1,21 +1,18 @@
-import { Client, Composer, types } from "mtkruto/mod.ts";
+import { Client, types } from "mtkruto/mod.ts";
 import { StorageDenoKV } from "mtkruto/storage/1_storage_deno_kv.ts";
 import env from "./env.ts";
 
 const kv = await Deno.openKv();
-const client = new Client(new StorageDenoKV("./client"), env.API_ID, env.API_HASH, { initialDc: "1" }); // the initialDc parameters makes sure that we connect to prod servers
-const composer = new Composer();
+const client = new Client(new StorageDenoKV(), env.API_ID, env.API_HASH, { initialDc: "1" }); // the initialDc parameters makes sure that we connect to prod servers
 
-client.handler = composer;
-
-composer.on("connectionState", async ({ connectionState }) => { // this is called when the client’s connection state is changed, and should be applied before starting the client
-  if (connectionState == "not-connected") {
+client.on("connectionState", async ({ connectionState }) => { // this is called when the client’s connection state is changed, and should be applied before starting the client
+  if (connectionState == "notConnected") {
     await new Promise((r) => setTimeout(r, 5000)); // try reconnecting after 5 seconds
     await client.start();
   }
 });
 
-composer.on("authorizationState", async ({ authorizationState: { authorized } }) => { // this is called when the client’s connection state is changed, and should be applied before authorizing the client
+client.on("authorizationState", async ({ authorizationState: { authorized } }) => { // this is called when the client’s connection state is changed, and should be applied before authorizing the client
   if (authorized) {
     const me = await client.getMe();
     console.log(`Running as @${me.username}...`);
@@ -24,7 +21,7 @@ composer.on("authorizationState", async ({ authorizationState: { authorized } })
 
 await client.start(env.BOT_TOKEN);
 
-composer.on("deletedMessages", async ({ deletedMessages }) => {
+client.on("deletedMessages", async ({ deletedMessages }) => {
   const deleted = new Array<number>();
   for (const message of deletedMessages) {
     if (message.chat.type != "private") {
@@ -47,14 +44,14 @@ composer.on("deletedMessages", async ({ deletedMessages }) => {
   }
 });
 
-composer.use(async (update, next) => {
+client.use(async (update, next) => {
   const msg = update.editedMessage ?? update.message;
   if (msg?.out === false) { // only handle incoming messages, outgoing ones are not interesting
     await next();
   }
 });
 
-composer.on("message", async ({ message }, next) => {
+client.on("message", async ({ message }, next) => {
   if (message.chat.type != "private") {
     return next();
   }
@@ -63,7 +60,7 @@ composer.on("message", async ({ message }, next) => {
   await kv.set(["message_references", message.id], forwardedMessage.id);
 });
 
-composer.on("editedMessage", async ({ editedMessage }, next) => {
+client.on("editedMessage", async ({ editedMessage }, next) => {
   if (editedMessage.chat.type != "private") {
     return next();
   }
@@ -71,14 +68,14 @@ composer.on("editedMessage", async ({ editedMessage }, next) => {
   await kv.set(["incoming_messages", forwardedMessage.id], [editedMessage.chat.id, editedMessage.id]);
 });
 
-composer.use(async (update, next) => {
+client.use(async (update, next) => {
   const msg = update.editedMessage ?? update.message;
   if (msg?.chat.type == "supergroup" && msg.chat.id == env.CHAT_ID) {
     await next();
   }
 });
 
-composer.on(["message", "text", "replyToMessage"], async ({ message }) => {
+client.on(["message", "text", "replyToMessage"], async ({ message }) => {
   const { value } = await kv.get<[number, number]>([
     "incoming_messages",
     message.replyToMessage.id,
@@ -91,7 +88,7 @@ composer.on(["message", "text", "replyToMessage"], async ({ message }) => {
   await kv.set(["outgoing_messages", message.id], [chatId, sentMessage.id]);
 });
 
-composer.on(["editedMessage", "text", "replyToMessage"], async ({ editedMessage }) => {
+client.on(["editedMessage", "text", "replyToMessage"], async ({ editedMessage }) => {
   const { value } = await kv.get<[number, number]>([
     "outgoing_messages",
     editedMessage.id,
